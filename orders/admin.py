765339c96472b2f4328f81b8e4c8449d71922ee3
Shaik Ameen <113741330+cap10america@users.noreply.github.com>
2025-08-04 21:14:48 +0530

    18529

--- orders/admin.py
+++ orders/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Menu ,Order
-# import the Menu and Order from the models.py file 
+
 
 admin.site.register(Menu)
-#register the Menu in admin panel
+
 admin.site.register(Order)
-# register the Order in admin panel
+
