8ea4a40feaa21fcea51ed1ca3001d70757756e74
Shaik Ameen <113741330+cap10america@users.noreply.github.com>
2025-08-04 21:11:58 +0530

    18526

--- home/views.py
+++ home/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from django.db import models
+# importing models 
+from django.contrib.auth.models import User
+# importing User model
+
 # Create your views here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User ,on_delete =models.CASCADE ,related_name = 'profile')
+    name =models.CharField(max_length =255)
+    email =models.EmailField()
+    phone_number =models.CharField(max_length =255)
+
+
+    def __str__(self):
+        return self.name
