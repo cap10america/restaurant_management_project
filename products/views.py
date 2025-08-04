d85bc5d1c2b5242f63fac6eeada3a495e2d8ca38
Shaik Ameen <113741330+cap10america@users.noreply.github.com>
2025-08-04 22:01:13 +0530

    18527

--- products/views.py
+++ products/views.py
@@ -24,3 +24,16 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class MenuView(APIView):
+    def get(self,request):
+        menus =Menu.objects.all()
+        serializer=MenuSerializer(menus,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = MenuSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
