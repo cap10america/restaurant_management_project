c35347d95d003cd484c84f4069fef26a1aa8ea16
Shaik Ameen <113741330+cap10america@users.noreply.github.com>
2025-08-06 00:02:41 +0530

    18528

--- products/models.py
+++ products/models.py
@@ -1,6 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.Model.DecimalField(max_digits=7,decimal_places=2)
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    STATUS_CHOICES =[('pending','pending'),('confirmed' ,'confirmed '),('delivered','delivered'),('cancelled','cancelled'),]
+    customer =models.ForeignKey(User,on_delete =models.CASCADE,related_name ='orders')
+    order_items =models.ManyToManyField(Menu,related_name='orders')
+    total_amount= models.DecimalField(max_digits=10,decimal_places=2)
+    status =models.CharField(max_length=20,choices =STATUS_CHOICES,default='pending')
+    ordered_at =models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer.username}"
 class Item(models.Model):
     item_name = models.CharField(max_length=150)
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
