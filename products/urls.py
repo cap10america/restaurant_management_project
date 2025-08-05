fbdcf30346b4bc2352835b86279fa0c22b3b9d72
Shaik Ameen <113741330+cap10america@users.noreply.github.com>
2025-08-06 00:56:19 +0530

    18530

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
     path('menu/',home_page,name='home_page'),
+    path('menu_s/',MenuAPIView.as_view(),name ='menu_s')
 ]
