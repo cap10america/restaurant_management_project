ef5bff9764ef0ada3cb4515599e1408603136ffa
Shaik Ameen <113741330+cap10america@users.noreply.github.com>
2025-08-09 14:48:15 +0530

    23087

--- products/models.py
+++ products/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length =255)
+    def __str__(self):
+        return self.name
+        
 class Menu(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7,decimal_places=2)
